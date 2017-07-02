a649ba682b30da4676f4007435e5f61dd4e3a57c
java2019 <java_2019@mail.ru>
2017-07-02 19:44:00 +0300

    Initial commit

---
+++ app/src/main/java/com/samples/camera/callactivities/CallCameraActivity.java
@@ -0,0 +1,13 @@
+package com.samples.camera.callactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CallCameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_call_camera);
+    }
+}
