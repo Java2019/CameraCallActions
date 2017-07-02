75a482ff1081b519be475afc314578069ebabc9e
java2019 <java_2019@mail.ru>
2017-07-02 19:54:17 +0300

    test1

--- app/src/main/java/com/samples/camera/callactivities/CallCameraActivity.java
+++ app/src/main/java/com/samples/camera/callactivities/CallCameraActivity.java
@@ -2,12 +2,32 @@ package com.samples.camera.callactivities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class CallCameraActivity extends AppCompatActivity {
+public class CallCameraActivity extends AppCompatActivity
+            implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_camera);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bImageCapture:
+
+                break;
+            case R.id.bStillImage:
+
+                break;
+            case R.id.bVideoCamera:
+
+                break;
+            case R.id.bVideoCapture:
+
+                break;
+        }
     }
 }
